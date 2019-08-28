e9e2004a94a1b058a65e01e02d77ee5ed382247d
Eric Levine <eric@berbix.com>
2019-08-28 15:45:25 -0700

    Fix up the release script.

--- release.sh
+++ release.sh
@@ -4,10 +4,10 @@ set -ex
 
 VERSION=$(cat version)
 
-sed -i "" -e "s/const SDK_VERSION = '[[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*';/const SDK_VERSION = '$VERSION';/g" src/index.jsx
-sed -i "" -e "s/  \"version\": \"[[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*\",/  \"version\": \"$VERSION\",/g" package.json
+sed -i "" -e "s/export const SDK_VERSION = \"[[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*\(-beta[[:digit:]]\)*\";/export const SDK_VERSION = \"$VERSION\";/g" src/BerbixVerify.jsx
+sed -i "" -e "s/  \"version\": \"[[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*\(-beta[[:digit:]]\)*\",/  \"version\": \"$VERSION\",/g" package.json
 
-git add package.json src/index.jsx version
+git add package.json src/*.jsx version
 git commit -m "Updating Berbix React Native SDK version to $VERSION"
 git tag -a $VERSION -m "Version $VERSION"
 git push --follow-tags
